7a8775050af7b4c667c0a76195ee1470a398cf17
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-06-14 15:37:31 +0200

    add files to deploy into cluster 2

--- cluster2/deploy-cluster2.sh
+++ cluster2/deploy-cluster2.sh
@@ -1,2 +1,9 @@
 export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-cluster2:0.0.1
-docker build . -t $NAME
+docker build -f cluster2/Dockerfile . -t $NAME
+
+docker login ls6-stud-registry.informatik.uni-wuerzburg.de
+docker push $NAME
+
+kubectl apply -f k8s-cluster2.yaml
+
+kubectl get pods --watch
