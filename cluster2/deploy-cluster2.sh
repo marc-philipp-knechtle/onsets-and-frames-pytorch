ff30ec49e3407494835ec1932816a6f681b965a4
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-06-17 08:39:49 +0200

    configuration for running cluster2

--- cluster2/deploy-cluster2.sh
+++ cluster2/deploy-cluster2.sh
@@ -1,9 +1,11 @@
-export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-cluster2:0.0.1
+kubectl delete job onsets-and-frames-pytorch-training-cluster2
+
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-cluster2:0.0.4
 docker build -f cluster2/Dockerfile . -t $NAME
 
 docker login ls6-stud-registry.informatik.uni-wuerzburg.de
 docker push $NAME
 
-kubectl apply -f k8s-cluster2.yaml
+kubectl apply -f cluster2/k8s-cluster2.yaml
 
 kubectl get pods --watch
