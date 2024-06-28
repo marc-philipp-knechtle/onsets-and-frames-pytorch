0a55c814701c514b51cb124562a80a75b1961ec5
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-06-28 19:01:00 +0200

    move cluster1 into separate directory

--- cluster1/deploy-k8s.sh
+++ cluster1/deploy-k8s.sh
@@ -3,10 +3,11 @@ kubectl delete job onsets-and-frames-pytorch-training
 
 
 export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-pytorch:0.0.1
-docker build . -t $NAME
+docker build -f cluster1/Dockerfile . -t $NAME
+
 docker login ls6-stud-registry.informatik.uni-wuerzburg.de
 docker push $NAME
 
-kubectl apply -f k8s-job.yaml
+kubectl apply -f cluster1/k8s-job.yaml
 
 kubectl get pods --watch
