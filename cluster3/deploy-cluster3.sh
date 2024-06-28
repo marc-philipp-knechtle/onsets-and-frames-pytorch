27bcb66e6eb608f0a7df094803478bbfd0b57e9d
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-06-28 19:00:32 +0200

    add cluster3

---
+++ cluster3/deploy-cluster3.sh
@@ -0,0 +1,11 @@
+kubectl delete job onsets-and-frames-pytorch-training-cluster3
+
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-cluster3:0.0.1
+docker build -f cluster2/Dockerfile . -t $NAME
+
+docker login ls6-stud-registry.informatik.uni-wuerzburg.de
+docker push $NAME
+
+kubectl apply -f cluster3/k8s-cluster3.yaml
+
+kubectl get pods --watch
