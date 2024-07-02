76bb14708af235439344995d677695d9c9819b5b
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-07-02 14:09:54 +0200

    increase tag on evaluation cluster

--- cluster-evaluation/deploy-cluster-evaluation.sh
+++ cluster-evaluation/deploy-cluster-evaluation.sh
@@ -1,6 +1,6 @@
 kubectl delete job onsets-and-frames-evaluation
 
-export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-evaluation:0.0.1
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-evaluation:0.0.2
 docker build -f cluster2/Dockerfile . -t $NAME
 
 docker login ls6-stud-registry.informatik.uni-wuerzburg.de
