9b251bf38a3c16896009df0b3147a0046162031e
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-07-03 15:45:38 +0200

    adding files for cluster4

---
+++ cluster4/deploy-cluster4.sh
@@ -0,0 +1,11 @@
+kubectl delete job onsets-and-frames-pytorch-training-cluster4
+
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-cluster4:0.0.1
+docker build --no-cache -f cluster2/Dockerfile . -t $NAME
+
+docker login ls6-stud-registry.informatik.uni-wuerzburg.de
+docker push $NAME
+
+kubectl apply -f cluster4/k8s-cluster4.yaml
+
+kubectl get pods --watch
