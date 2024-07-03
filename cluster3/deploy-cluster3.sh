3eb7ecaae8cc0c4beb69f8269ed829c847a45c61
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-07-03 21:10:13 +0200

    increase image tag on cluster3

--- cluster3/deploy-cluster3.sh
+++ cluster3/deploy-cluster3.sh
@@ -1,6 +1,6 @@
 kubectl delete job onsets-and-frames-pytorch-training-cluster3
 
-export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-cluster3:0.0.1
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-cluster3:0.0.2
 docker build --no-cache -f cluster2/Dockerfile . -t $NAME
 
 docker login ls6-stud-registry.informatik.uni-wuerzburg.de
