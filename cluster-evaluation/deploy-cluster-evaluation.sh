4a91882a76dfa053cbeae6d1d914e698b1c998cc
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-07-26 16:33:55 +0200

    increase image tag

--- cluster-evaluation/deploy-cluster-evaluation.sh
+++ cluster-evaluation/deploy-cluster-evaluation.sh
@@ -1,7 +1,7 @@
 kubectl delete job onsets-and-frames-evaluation
 
 export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-evaluation:0.0.3
-docker build --no-cache -f cluster2/Dockerfile . -t $NAME
+docker build -f cluster2/Dockerfile . -t $NAME
 
 docker login ls6-stud-registry.informatik.uni-wuerzburg.de
 docker push $NAME
