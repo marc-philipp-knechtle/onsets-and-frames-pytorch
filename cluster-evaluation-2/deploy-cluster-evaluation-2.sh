6e0e86037d58f09cd8730f85133320febe78ba50
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-07-26 18:57:36 +0200

    add files for cluster-evaluation-2

---
+++ cluster-evaluation-2/deploy-cluster-evaluation-2.sh
@@ -0,0 +1,11 @@
+kubectl delete job onsets-and-frames-evaluation-2
+
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-evaluation-2:0.0.3
+docker build -f cluster2/Dockerfile . -t $NAME
+
+docker login ls6-stud-registry.informatik.uni-wuerzburg.de
+docker push $NAME
+
+kubectl apply -f cluster-evaluation-2/k8s-cluster-evaluation-2.yaml
+
+kubectl get pods --watch
