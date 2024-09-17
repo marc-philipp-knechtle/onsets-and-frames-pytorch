89c8c5918bdcb110723d99f9e92354378d711df8
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-09-17 08:48:32 +0200

    add configurations for retraining the SchubertWinterreiseModels with Early Stopping

--- cluster4/deploy-cluster4.sh
+++ cluster4/deploy-cluster4.sh
@@ -1,6 +1,6 @@
 kubectl delete job onsets-and-frames-pytorch-training-cluster4
 
-export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-cluster4:0.0.2
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-cluster4:0.0.4
 docker build --no-cache -f cluster2/Dockerfile . -t $NAME
 
 docker login ls6-stud-registry.informatik.uni-wuerzburg.de
