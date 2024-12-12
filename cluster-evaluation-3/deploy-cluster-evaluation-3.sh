41919ac2a63bbd068e6db550550b6e12bcea63da
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-12-12 11:38:21 +0100

    update cluster deployments

--- cluster-evaluation-3/deploy-cluster-evaluation-3.sh
+++ cluster-evaluation-3/deploy-cluster-evaluation-3.sh
@@ -1,6 +1,6 @@
 kubectl delete job onsets-and-frames-evaluation-3
 
-export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-evaluation-3:0.0.4
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/extch1-onsets-and-frames-evaluation-3:0.0.6
 docker build --no-cache -f cluster2/Dockerfile . -t $NAME
 
 docker login ls6-stud-registry.informatik.uni-wuerzburg.de
