3fdf6433b1bb3ccc116a963ca562006a661a881b
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-02-13 10:04:29 +0100

    add all files for second traning cluster

---
+++ cluster-training-2/deploy-onsets-and-frames-training-2.sh
@@ -0,0 +1,15 @@
+git push
+
+kubectl delete job onsets-and-frames-training-2
+
+ssh "vingilot" "cd /home/ext/ch1/studentMPK/onsets-and-frames-training/onsets-and-frames-pytorch && git pull && exit"
+
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/onsets-and-frames-training-2:0.0.1
+docker build -f cluster-training/Dockerfile . -t $NAME
+
+docker login ls6-stud-registry.informatik.uni-wuerzburg.de
+docker push $NAME
+
+kubectl apply -f cluster-training-2/onsets-and-frames-training-2.yaml
+
+watch kubectl get pods
