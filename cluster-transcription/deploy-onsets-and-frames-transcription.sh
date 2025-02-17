781fcf581791e908d5f42f1b15729045bacae1bb
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-02-17 12:02:53 +0100

    add cluster-transcription

---
+++ cluster-transcription/deploy-onsets-and-frames-transcription.sh
@@ -0,0 +1,15 @@
+git push
+
+kubectl delete job onsets-and-frames-transcription
+
+ssh "vingilot" "cd /home/ext/ch1/studentMPK/cluster-transcription/onsets-and-frames-pytorch && git pull && exit"
+
+export NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/onsets-and-frames-transcription:0.0.1
+docker build -f cluster-training/Dockerfile . -t $NAME
+
+docker login ls6-stud-registry.informatik.uni-wuerzburg.de
+docker push $NAME
+
+kubectl apply -f cluster-transcription-2/k8s-onsets-and-frames-transcription.yaml
+
+watch kubectl get pods
